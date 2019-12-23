8eb320bf3e5fcfc622ba033705ee5db646454c5b
lrh <lirh@gmail.com>
2019-12-23 21:24:57 +0800

    add logback配置

--- logback-demo/src/main/java/com/hyxt/LogbackDemoApplication.java
+++ logback-demo/src/main/java/com/hyxt/LogbackDemoApplication.java
@@ -1,13 +1,31 @@
 package com.hyxt;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LogbackDemoApplication {
+@Slf4j
+public class LogbackDemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogbackDemoApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+		for (int i = 0; i < 10; i++) {
+			log();
+			Thread.sleep(500);
+		}
+	}
+
+	private void log(){
+		log.trace("===trace===");
+		log.debug("===debug===");
+		log.info("===info===");
+		log.warn("===warn===");
+		log.error("===error===");
 	}
 
 }
